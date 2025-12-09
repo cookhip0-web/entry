dd32df164dc29553f07081b70cff37ca994147c5
Ruby <cookhip0@gmail.com>
2025-12-08 23:13:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const buttons = document.querySelectorAll('.experience-btn');
-
-buttons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const img = btn.parentElement.querySelector('img');
-    const whiteSrc = btn.dataset.white;
-    const colorSrc = btn.dataset.color;
-
-    // 원래 이미지로 돌아오는 기능 추가
-    img.src = colorSrc; // 클릭 즉시 컬러 이미지
-    setTimeout(() => {
-      img.src = whiteSrc; // 3초 후 원래 흑백 이미지로
-    }, 2000);
-  });
-});
+<script>
+    function animateText(id) {
+      const el = document.getElementById(id);
+      el.classList.remove('animate-text'); // reset animation
+      void el.offsetWidth; // trigger reflow
+      el.classList.add('animate-text');
+    }
+  </script>
